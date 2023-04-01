18d1740103450e78fb7470230d86624858b6b65b
Shindou Mihou <git@mihou.pw>
2023-04-01 08:57:43 +0800

    update(milk): do not error and ignore no-user raid logs.
    Signed-off-by: Shindou Mihou <git@mihou.pw>

--- milk/src/routes/GetAntispam.ts
+++ milk/src/routes/GetAntispam.ts
@@ -1,5 +1,4 @@
 import {prisma, TAG} from "../index.js";
-import * as Sentry from "@sentry/node";
 import {Logger} from "@beemobot/common";
 // ^ This needs to be updated; Probably @beemobot/cafe
 import {DateUtil} from "../utils/date.js";
@@ -25,32 +24,27 @@ const attach = (server: FastifyInstance) => {
 
             const users = await prisma.raidUser.findMany({ where: { internal_raid_id: raid.internal_id } })
 
-            // IMPORTANT: Impossible situation unless Tea sent milk an empty list for raid users which is weird.
-            if (users.length === 0) {
-                Sentry.captureException(new Error("Raid " + id + " has zero users on collect, impossible situation."))
-                Logger.error(TAG, 'Raid ' + id + ' has zero users on collect, this should not be possible!')
-
-                return reply.code(404).send('404 Not Found')
-            }
-
             let response = 'Userbot raid detected against server ' + raid.guild_id + ' on ' + DateUtil.toDateString(users[0].joined_at);
-            response += '\nRaid size: ' + users.length + ' accounts'
-            response += '\n'
-            response += '\n   Joined at:              ID:             Username:'
-            response += '\n'
+            if (users.length === 0) {
+                Logger.warn(TAG, "Raid " + id + "  reported no users.")
+                response += "\nThere are no users logged for this raid, at this moment. It is likely that the raid is still being processed, please come back later!"
+            } else {
+                response += '\nRaid size: ' + users.length + ' accounts'
+                response += '\n'
+                response += '\n   Joined at:              ID:             Username:'
+                response += '\n'
+                let userIds = '';
+                for (const user of users) {
+                    response += DateUtil.toTimeString(user.joined_at) + '   ' + user.user_id + '  ' + user.name
+                    userIds += user.user_id
+                }
 
-            let userIds = '';
-            for (const user of users) {
-                response += DateUtil.toTimeString(user.joined_at) + '   ' + user.user_id + '  ' + user.name
-                userIds += user.user_id
+                response += '\n'
+                response += '\n     Raw IDs:'
+                response += '\n'
+                response += userIds
+                AntispamLogsCache.set(id, response)
             }
-
-            response += '\n'
-            response += '\n     Raw IDs:'
-            response += '\n'
-            response += userIds
-
-            AntispamLogsCache.set(id, response)
             return reply.send(response)
         } catch (ex) {
             console.error(ex)
