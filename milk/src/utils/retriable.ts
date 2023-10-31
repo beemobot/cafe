99821b149f15c443fe7f6b6365f99b0296db1508
Shindou Mihou <git@mihou.pw>
2023-10-31 22:55:55 +0800

    feat(milk): use `async` for `retriable`

--- milk/src/utils/retriable.ts
+++ milk/src/utils/retriable.ts
@@ -3,20 +3,21 @@ import {Logger} from "@beemobot/common";
 // ^ This needs to be updated; Probably @beemobot/cafe
 import {TAG} from "../index.js";
 
-export function retriable<T>(task: string, action: () => Promise<T>, retryEverySeconds: number = 10, maxRetries: number = -1, retries: number = 1): Promise<T> {
-    return action()
-        .catch(async (exception) => {
-            if (retries === 1) {
-                Sentry.captureException(exception)
-            }
+export async function retriable<T>(task: string, action: () => Promise<T>, retryEverySeconds: number = 10, maxRetries: number = -1, retries: number = 1): Promise<T> {
+    try {
+        return await action();
+    } catch (exception) {
+        if (retries === 1) {
+            Sentry.captureException(exception)
+        }
 
-            if (maxRetries !== -1 && retries >= maxRetries) {
-                return exception
-            }
+        if (maxRetries !== -1 && retries >= maxRetries) {
+            throw exception
+        }
 
-            Logger.error(TAG, 'Failed to complete ' + task + '. Retrying in ' +  (retryEverySeconds * retries) + ' seconds.\n', exception)
-            await new Promise((resolve) => setTimeout(resolve, (retryEverySeconds * retries) * 1000))
-            return retriable(task, action, retryEverySeconds, retries + 1)
-        })
+        Logger.error(TAG, 'Failed to complete ' + task + '. Retrying in ' + (retryEverySeconds * retries) + ' seconds.\n', exception)
+        await new Promise((resolve) => setTimeout(resolve, (retryEverySeconds * retries) * 1000))
+        return retriable(task, action, retryEverySeconds, retries + 1)
+    }
 }
 
