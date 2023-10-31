de3e460a55313927d1c9e2f3f43e0181e015804a
Shindou Mihou <git@mihou.pw>
2023-10-31 23:03:20 +0800

    feat(milk): simplify the code for `run` (formerly `retry`)

--- milk/src/kafka/clients/raids.ts
+++ milk/src/kafka/clients/raids.ts
@@ -2,7 +2,7 @@ import {BrokerClient, KafkaConnection, Logger} from "@beemobot/common";
 // ^ This needs to be updated; Probably @beemobot/cafe
 import {prisma, TAG} from "../../index.js";
 import {randomString} from "../../utils/string.js";
-import {retriable} from "../../utils/retriable.js";
+import {run} from "../../utils/retry.js";
 import {RaidManagementData} from "../../types/raid.js";
 
 export const KEY_BATCH_INSERT_RAID_USERS = "batch-insert-raid-users"
@@ -30,7 +30,7 @@ export class RaidManagementClient extends BrokerClient<RaidManagementData> {
                     }
                 })
 
-                await retriable(
+                await run(
                     'insert_raid_users',
                     async () => prisma.raidUser.createMany({data: users, skipDuplicates: true}),
                     2,
@@ -41,7 +41,7 @@ export class RaidManagementClient extends BrokerClient<RaidManagementData> {
             let raid = await prisma.raid.findUnique({where: {internal_id: request.raidId}})
             if (raid == null) {
                 Logger.info(TAG, `Creating raid ${request.raidId} from guild ${request.guildIdString}.`)
-                raid = await retriable(
+                raid = await run(
                     'create_raid',
                     async () => prisma.raid.create({
                         data: {
@@ -57,7 +57,7 @@ export class RaidManagementClient extends BrokerClient<RaidManagementData> {
             } else {
                 if (request.concludedAt != null && (raid.concluded_at == null || raid.concluded_at !== new Date(request.concludedAt))) {
                     Logger.info(TAG, `Concluding raid ${request.raidId} from guild ${request.guildIdString}.`)
-                    raid = await retriable(
+                    raid = await run(
                         'conclude_raid',
                         async () => prisma.raid.update({
                             where: { external_id: raid!.external_id, internal_id: request.raidId },
---
+++ milk/src/utils/retry.ts
@@ -0,0 +1,32 @@
+import * as Sentry from '@sentry/node';
+import {Logger} from "@beemobot/common";
+// ^ This needs to be updated; Probably @beemobot/cafe
+import {TAG} from "../index.js";
+
+export async function run<T>(
+    taskName: string,
+    action: () => Promise<T>,
+    retryEverySeconds: number = 2,
+    maxRetries: number = 25,
+    retries: number = 1
+): Promise<T> {
+    try {
+        return await action();
+    } catch (exception) {
+        // Capture exception once to Sentry, we don't want to possibly send so many exceptions
+        if (retries === 1) {
+            Sentry.captureException(exception)
+        }
+
+        if (maxRetries !== -1 && retries >= maxRetries) {
+            throw exception
+        }
+
+        const secondsTillRetry = (retryEverySeconds * retries)
+        Logger.error(TAG, `Failed to complete ${taskName}. Retrying in ${secondsTillRetry} seconds.`, exception)
+
+        await new Promise((resolve) => setTimeout(resolve, secondsTillRetry * 1000))
+        return run(taskName, action, retryEverySeconds, retries + 1)
+    }
+}
+
