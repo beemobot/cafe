6de5dbc34a42b34c30e30fcbf8a6a1647f58da0d
Adrian Paschkowski <git@wasdennnoch.me>
2024-01-14 14:49:54 +0100

    Fix LocalConnection external dispatch check

--- latte/src/main/java/gg/beemo/latte/broker/LocalConnection.kt
+++ latte/src/main/java/gg/beemo/latte/broker/LocalConnection.kt
@@ -1,9 +1,10 @@
 package gg.beemo.latte.broker
 
-class LocalConnection : BrokerConnection() {
+class LocalConnection(
+    override val serviceName: String,
+    override val instanceId: String,
+) : BrokerConnection() {
 
-    override val serviceName = "local-service-name"
-    override val instanceId = "local-instance"
     override val supportsTopicHotSwap = true
 
     override suspend fun abstractSend(
@@ -12,15 +13,18 @@ class LocalConnection : BrokerConnection() {
         value: String,
         headers: BrokerMessageHeaders,
     ): MessageId {
-        if (
-            shouldDispatchExternallyAfterShortCircuit(topic, key, value, headers) &&
-            (headers.targetServices.isNotEmpty() || headers.targetInstances.isNotEmpty())
-        ) {
-            throw IllegalArgumentException(
-                "Attempting to send message to other services/instances " +
-                        "(services=${headers.targetServices}; instances=${headers.targetInstances}) " +
-                        "in a LocalConnection"
-            )
+        if (shouldDispatchExternallyAfterShortCircuit(topic, key, value, headers)) {
+            val isForExternalService =
+                headers.targetServices.isNotEmpty() && headers.targetServices != setOf(serviceName)
+            val isForExternalInstance =
+                headers.targetInstances.isNotEmpty() && headers.targetInstances != setOf(instanceId)
+            if (isForExternalService || isForExternalInstance) {
+                throw IllegalArgumentException(
+                    "Attempting to send message to other services/instances " +
+                            "(services=${headers.targetServices}; instances=${headers.targetInstances}) " +
+                            "in a LocalConnection"
+                )
+            }
         }
 
         return headers.messageId
