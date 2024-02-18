801269a553f1ef49633d506d7e7337f096500af5
Adrian Paschkowski <git@wasdennnoch.me>
2024-02-18 12:41:53 +0100

    Add prettier

--- water/src/broker/BrokerMessage.ts
+++ water/src/broker/BrokerMessage.ts
@@ -1,18 +1,15 @@
 import type { BrokerClient } from "./BrokerClient.js";
 import type { IBrokerMessageHeaders } from "./IBrokerMessageHeaders.js";
 
-
 export class BrokerMessage<T> {
+	public constructor(
+		public readonly client: BrokerClient<T>,
+		public readonly key: string,
+		public readonly value: T | null,
+		public readonly headers: IBrokerMessageHeaders
+	) {}
 
-    public constructor(
-        public readonly client: BrokerClient<T>,
-        public readonly key: string,
-        public readonly value: T | null,
-        public readonly headers: IBrokerMessageHeaders,
-    ) { }
-
-    public async respond(data: T | null): Promise<void> {
-        await this.client._respond(this, data);
-    }
-
+	public async respond(data: T | null): Promise<void> {
+		await this.client._respond(this, data);
+	}
 }
--- water/src/util/CountDownLatch.ts
+++ water/src/util/CountDownLatch.ts
@@ -1,7 +1,6 @@
 type PromiseResolver = (timeout: boolean) => void;
 
 export class CountDownLatch {
-
     private counter: number;
     private resolvers = new Map<PromiseResolver, NodeJS.Timeout | null>();
 
@@ -42,20 +41,17 @@ export class CountDownLatch {
         }
 
         let resolver: PromiseResolver;
-        const promise = new Promise<boolean>(r => {
-            resolver = r;
-            this.resolvers.set(r, null);
-        });
-        if (!waitTime) {
-            return promise;
-        }
 
-        const timeout = setTimeout(() => {
+        const timeout = waitTime ? setTimeout(() => {
             this.resolvers.delete(resolver);
             resolver(false);
-        }, waitTime);
-        timeout.unref();
+        }, waitTime) : null;
+        timeout?.unref();
+
+        const promise = new Promise<boolean>(r => {
+            resolver = r;
+            this.resolvers.set(r, timeout);
+        });
         return promise;
     }
-
 }
