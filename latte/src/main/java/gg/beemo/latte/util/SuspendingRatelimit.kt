c1cace45d2bafea9d5517c4d66c3cab3cdda65ee
Shindou Mihou <git@mihou.pw>
2024-08-05 05:14:11 +0800

    feat(latte): add `tryRequestQuota` for applications that do not need to wait for the quota

--- latte/src/main/java/gg/beemo/latte/util/SuspendingRatelimit.kt
+++ latte/src/main/java/gg/beemo/latte/util/SuspendingRatelimit.kt
@@ -6,32 +6,56 @@ import kotlinx.coroutines.sync.withPermit
 import kotlin.time.Duration
 
 class SuspendingRatelimit(private val burst: Int, private val duration: Duration) {
-
     @Volatile
     private var remainingQuota: Int = burst
+
     @Volatile
     private var resetTimestamp: Long = 0
 
     private val quotaRequestSem = Semaphore(1)
 
-    fun overrideRatelimit(remainingQuota: Int, resetTimestamp: Long) {
+    fun overrideRatelimit(
+        remainingQuota: Int,
+        resetTimestamp: Long,
+    ) {
         this.remainingQuota = remainingQuota
         this.resetTimestamp = resetTimestamp
+    }
+
+    private fun calculateWaitTime(): Long {
+        return (resetTimestamp - System.currentTimeMillis()).coerceAtLeast(0)
+    }
+
+    private fun tryResetQuota() {
+        if (System.currentTimeMillis() >= resetTimestamp) {
+            remainingQuota = burst
+            resetTimestamp = System.currentTimeMillis() + duration.inWholeMilliseconds
+        }
     }
 
     suspend fun requestQuota() {
         quotaRequestSem.withPermit {
             if (remainingQuota <= 0) {
-                val waitTime = (resetTimestamp - System.currentTimeMillis()).coerceAtLeast(0)
+                val waitTime = calculateWaitTime()
                 delay(waitTime)
             }
-            if (System.currentTimeMillis() >= resetTimestamp) {
-                remainingQuota = burst
-                resetTimestamp = System.currentTimeMillis() + duration.inWholeMilliseconds
-            }
+            tryResetQuota()
+
             check(remainingQuota > 0)
             remainingQuota--
         }
     }
 
+    suspend fun tryRequestQuota(): Pair<Boolean, Long?> =
+        quotaRequestSem.withPermit {
+            if (remainingQuota <= 0) {
+                val waitTime = calculateWaitTime()
+                return@withPermit false to waitTime
+            }
+            tryResetQuota()
+
+            check(remainingQuota > 0)
+            remainingQuota--
+            return@withPermit true to null
+        }
 }
